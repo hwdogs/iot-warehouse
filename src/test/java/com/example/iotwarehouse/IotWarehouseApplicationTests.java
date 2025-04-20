4e5648a445a4a712eeb540f70c790efe91de10b6
HuangWS <3070358878@qq.com>
2025-04-21 00:24:43 +0800

    IOTW initial

---
+++ src/test/java/com/example/iotwarehouse/IotWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
