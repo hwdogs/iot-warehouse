e178e50e26d931627835780a1565bd6639249134
HuangWS <3070358878@qq.com>
2025-04-23 11:36:42 +0800

    openApi注解优化

--- src/main/java/com/example/iotwarehouse/entity/Warehouse.java
+++ src/main/java/com/example/iotwarehouse/entity/Warehouse.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.IOException;
@@ -36,11 +37,13 @@ public class Warehouse implements Serializable {
      * 仓库ID
      */
     @TableId(value = "warehouse_id", type = IdType.ASSIGN_ID)
+    @Schema(description = "雪花算法仓库ID",requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer warehouseId;
 
     /**
      * 仓库名称
      */
+    @Schema(description = "仓库名称", example = "华北中心仓", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
 
     /**
@@ -48,19 +51,23 @@ public class Warehouse implements Serializable {
      */
     @JsonSerialize(using = LocationSerializer.class)
     @JsonDeserialize(using = LocationDeserializer.class)
+    @Schema(description = "地理位置坐标（'纬度 经度'格式）", example = "39.9042 116.4074", requiredMode = Schema.RequiredMode.REQUIRED)
     private byte[] location;
 
     /**
      * 面积（平方米）
      */
+    @Schema(description = "仓库面积",example = "250",requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private BigDecimal area;
 
+    @Schema(description = "仓库环境区（'常温区','冷链区','危险品区'）",example = "危险区",requiredMode = Schema.RequiredMode.REQUIRED)
     private String environmentZone;
 
     /**
      * 0：存在，1：逻辑删除
      */
     @TableField(fill = FieldFill.INSERT)
+    @Schema(description = "逻辑删除",hidden = true,requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer deleted = 0;
 
     /**
